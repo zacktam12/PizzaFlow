a53d396a77be9d8b14414440398b3e6d797217d6
hp <zekariastamiru12@gmail.com>
2025-01-24 21:54:38 +0300

    styling button element states and transition

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,9 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function EmptyCart() {
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <Link to="/menu" classn>
+        &larr; Back to menu
+      </Link>
 
       <p>Your cart is still empty. Start adding some pizzas :)</p>
     </div>
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -5,7 +5,7 @@ import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -77,7 +77,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-700 hover:bg-yellow-300 focus:inline-block focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
