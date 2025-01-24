8763554cbecd1625528d37b28252b7104e1379db
hp <zekariastamiru12@gmail.com>
2025-01-24 03:06:23 +0300

    Enhance CartOverview and Header components

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,5 @@
+function UserName() {
+  return <div className="text-sm font-semibold">Zack</div>;
+}
+
+export default UserName;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/userName";
 
 function Header() {
   return (
-    <div className="bg-yellow-500">
-      <Link to="./">Fast react pizza Co.</Link>
+    <div className="bg-yellow-500 uppercase">
+      <Link className="tracking-[0.5rem]" to="./">
+        Fast react pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Zack</p>
+      <UserName />{" "}
     </div>
   );
 }
