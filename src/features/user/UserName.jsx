cc1d303f43844ee142b82bb267b004facf8d05fb
hp <zekariastamiru12@gmail.com>
2025-01-26 01:18:51 +0300

    add Redux for state management and implement user slice

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Zack</div>;
+  const username = useSelector((state) => state.user.username);
+  if (username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default UserName;
