0b0867cd234e75f1900ae1fa3d4b29b4f2cb7cb2
hp <zekariastamiru12@gmail.com>
2025-01-25 09:57:42 +0300

    refactor button component to support different styles and update usages across the application

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-700 hover:bg-yellow-300 focus:inline-block focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-4 sm:py-3 ";
+function Button({ children, disabled, to, type }) {
+  const Base =
+    "rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-700 hover:bg-yellow-300 focus:inline-block focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed  ";
+
+  const Styles = {
+    primary: Base + " px-4 py-3 md:px-6 md:py-4",
+    small: Base + " px-4 py-2 md:px-5 sm:py-2.5 ",
+  };
 
   if (to) {
     return (
-      <Link className={className} to={to}>
+      <Link className={Styles[type]} to={to}>
         {children}
       </Link>
     );
   }
   return (
-    <button className={className} disabled={disabled}>
+    <button className={Styles[type]} disabled={disabled}>
       {children}
     </button>
   );
