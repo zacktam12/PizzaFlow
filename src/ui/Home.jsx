2b9bcd077d70db71d2e3bdd4f7bb7d6b2d549ff5
hp <zacktam12@users.noreply.github.com>
2025-04-13 23:49:07 +0300

    refactor: adjust class order in  add Netlify configuration file

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
-        The best pizza.
+        The best pizza .
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
