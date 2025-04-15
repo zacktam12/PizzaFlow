938b51ac8a0345bd9676cb0041b7727d73973799
hp <zacktam12@users.noreply.github.com>
2025-04-15 23:59:26 +0300

    comment

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -15,7 +15,7 @@ function CartItem({ item }) {
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
         <UpdateItemQuantity
-          pizzaId={pizzaId}
+          // pizzaId={pizzaId}jjj
           currentQuantity={currentQuantity}
         />
         <DeleteItem pizzaId={pizzaId} />
