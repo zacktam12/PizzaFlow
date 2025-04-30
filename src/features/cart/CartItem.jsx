ff327ab0dea461b63c4b5db21e9c5c65bf9c6bba
hp <zacktam12@users.noreply.github.com>
2025-04-30 23:08:01 +0300

    add pizzaid to cartItem

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -15,7 +15,7 @@ function CartItem({ item }) {
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
         <UpdateItemQuantity
-          // pizzaId={pizzaId}jjj
+          pizzaId={pizzaId}
           currentQuantity={currentQuantity}
         />
         <DeleteItem pizzaId={pizzaId} />
