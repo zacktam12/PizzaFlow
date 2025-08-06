7dd994bb910b2a747f1c569f4d62350ab060431c
zacktam12 <zekariastamiru12@gmail.com>
2025-08-06 07:52:20 +0300

    style: update layout and text size in EmptyCart component for enhanced readability

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -2,9 +2,9 @@ import LinkButton from "../../ui/LinkButton";
 
 function EmptyCart() {
   return (
-    <div className="px-4 py-3">
+    <div className="px-4 py-3 sm:px-6">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
-      <p className="mt-7 font-semibold">
+      <p className="mt-6 text-base font-semibold sm:mt-7 sm:text-lg">
         Your cart is still empty. Start adding some pizzas :)
       </p>
     </div>
