24dde00b2c49bc6fa36680093137adfcfee1db50
zacktam12 <zekariastamiru12@gmail.com>
2025-08-06 07:52:03 +0300

    style: refine layout and text size in CartOverview component for improved readability

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -12,12 +12,12 @@ function CartOverview() {
   if (!totalCartQuantity) return null;
 
   return (
-    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+    <div className="flex items-center justify-between bg-stone-800 px-4 py-3 text-xs uppercase text-stone-200 sm:px-6 sm:text-sm md:text-base">
       <p className="font-semibold text-stone-300">
         <span>{totalCartQuantity} pizzas </span>
         <span>{formatCurrency(totalCartPrice)}</span>
       </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+      <Link to="/cart" className="hover:text-stone-300">Open cart &rarr;</Link>
     </div>
   );
 }
