683f44c070a16a6d1d214b87fffd1688eb33427c
zacktam12 <zekariastamiru12@gmail.com>
2025-08-07 17:19:57 +0300

    style: adjust input width and padding in SearchOrder for improved layout

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -17,7 +17,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="focus-yellow-200 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 sm:focus:w-72"
+        className="w-24 rounded-full bg-yellow-100 px-3 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-48 sm:px-4 sm:focus:w-56 md:w-64 md:focus:w-72"
       />
     </form>
   );
