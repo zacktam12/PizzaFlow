70b4a64fdf2af762879c1ec34a53edf7626acba6
zacktam12 <zekariastamiru12@gmail.com>
2025-08-08 08:44:37 +0300

    style: refactor Header component layout for improved structure and consistency

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import UserName from "../features/user/userName";
+import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <div className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 font-pizza uppercase sm:px-6">
+    <div className="flex flex-col gap-3 border-b border-stone-500 bg-yellow-500 px-4 py-3 font-pizza uppercase sm:flex-row sm:items-center sm:justify-between sm:gap-0 sm:px-6">
       <Link className="tracking-wider" to="./">
         Fast react pizza Co.
       </Link>
-      <SearchOrder />
-      <UserName />{" "}
+      <div className="flex items-center justify-between gap-2">
+        <SearchOrder />
+        <UserName />
+      </div>
     </div>
   );
 }
