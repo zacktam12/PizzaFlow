0d0248df35494c0f1eaf2542a865d36b0d650a42
zacktam12 <zekariastamiru12@gmail.com>
2025-10-13 00:16:52 +0300

    fix: update Tailwind CSS configuration for font family syntax and remove unnecessary comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      pizza: "Roboto Mono  , monospace",
+      pizza: "Roboto Mono, monospace",
     },
     extend: {
       fontSize: { huge: ["80rem", { lineHeight: "1" }] },
-
       height: {
         screen: "100dvh",
       },
