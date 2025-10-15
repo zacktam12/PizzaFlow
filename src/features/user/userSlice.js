b69820c48c33e431ad8fb18dd7690a342eb6910a
zacktam12 <zekariastamiru12@gmail.com>
2025-10-15 17:17:31 +0300

    fix: correct import path for geocoding service in userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getAddress } from "../../services/apiGeoCoding";
+import { getAddress } from "../../services/apiGeocoding";
 
 function getPosition() {
   return new Promise(function (resolve, reject) {
